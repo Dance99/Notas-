ceeed5289a80afe333785eecdaab0514e21dc149
Daniela <nam991019@gmail.com>
2024-10-31 17:47:49 -0500

    console prom notas

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ let nota2 =parseFloat(prompt("ingresa la nota del 2do periodo"))
 let nota3 =parseFloat(prompt("ingresa la nota del 3er periodo"))
 //calcular prom//
 let promedio =(nota1 + nota2 + nota3)/3;
+//muestra el promedio en la consola"
+console.log("el promedio de las notas es:" + promedio)
